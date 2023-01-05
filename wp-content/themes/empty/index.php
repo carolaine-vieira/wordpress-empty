bfcb6757a4b711f5160415bce43c623906ab3899
Carolaine Vieira <s.carolaine.v@gmail.com>
2023-01-05 20:00:23 -0300

    build: final settings in theme build

--- wp-content/themes/empty/index.php
+++ wp-content/themes/empty/index.php
@@ -4,6 +4,13 @@ get_header();
 
 ?>
 
+<section id="main" class="main">
+	<div class="main__content">
+		<h1>Empty Theme</h1>
+
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, impedit! Culpa facilis veniam corporis eveniet voluptate! Corrupti, labore maxime ipsam nihil mollitia, soluta sed vitae ea porro cupiditate repellat exercitationem.
+	</div>
+</section>
 
 <?php
 
